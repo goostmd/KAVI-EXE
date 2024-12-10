a96c71ec4d6bf31456541b90fe8b8bf1b0b8868a
goostmd <arosharukshan704@gmail.com>
2024-12-10 06:35:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
+6const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
-}
+
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "kp5XiJKB#kG4TCU1qbZXVVvUzQB6DUTttBjo8u0Nrdg5SsJ498DA",
+SESSION_ID: process.env.SESSION_ID || "hso1lKoI#VgEc26RSIa4MuZq2DuA8BvLi4ruz2Kv6bQt4NWJdh4w",
 ALIVE_REACT: process.env.ALIVE_REACT || "👋",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://pomf2.lain.la/f/pkuujaup.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "KAVI EXE_V1_Connected ☑️_📂𝙳𝙴𝙿𝙻𝚈 𝙱𝚈 KAVI_EXE📘☀",
